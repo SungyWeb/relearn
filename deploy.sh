68d609691fbae61a8c36c409ef5b7bca4d27551a
sunguoyuan <503391278@qq.com>
2021-01-18 15:40:32 +0800

    upda

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+function check_result(){
+  if [ "$1" != "0" ]
+  then
+    echo "出错了，请检查！";
+    exit 2;
+  fi
+}
+
+cd ~/Desktop/Private/SungyWeb.github.io;
+git pull;
+rm -rf ~/Desktop/Private/SungyWeb.github.io/*;
+check_result $?;
+cp -rf ~/Desktop/Private/relearn/dist/* ~/Desktop/Private/SungyWeb.github.io/;
+check_result $?;
+
+git add -A;
+git commit -m "update";
+git push;
+check_result $?;
